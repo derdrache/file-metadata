f525430eb84adb014a9b07f293b8b0f973ae2158
Dominik Mast <dominik-mast@web.de>
2017-04-07 12:00:56 +0000

    api is ready

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var app = express();
+var multer = require("multer");
+var upload = multer({dest: "client/uploads/"}) // wo die datei abgelegt wird
 
 
 app.use(express.static(__dirname+"/client"))
+
+app.post("/", upload.any(), function (req,res){
+    res.send({"size":req.files[0].size});
+    
+})
 
 
 app.listen((process.env.PORT||8080|| 5000), function(){
