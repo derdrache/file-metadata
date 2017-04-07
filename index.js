d15db39063b2146d1dad0fc06ffbad6589d690b0
Dominik Mast <dominik-mast@web.de>
2017-04-07 08:36:14 +0000

    full stack prepared

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require("express");
 var app = express();
 
 
-
-app.get("/", function(req, res){
-  res.send("hello world")
-});
-
+app.use(express.static(__dirname+"/client"))
 
 
 app.listen((process.env.PORT||8080|| 5000), function(){
